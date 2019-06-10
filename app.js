d0c0598aac862658977553e34488a8258a79674e
hulane <huulane@gmail.com>
2019-06-10 14:53:17 +0800

    modify book api

---
+++ app.js
@@ -0,0 +1,54 @@
+/* eslint-env node */
+const { log } = require('./src/utils')
+
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const session = require('express-session')
+// 引入第三方插件
+
+// 启动服务
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+// 设置 session
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'hello',
+  resave: true,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}))
+
+// 设置路由
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
+app.use('/login', require('./routes/login.js'))
+app.use('/api', require('./routes/api.js'))
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+log('服务器已启动', 'http://127.0.0.1:3000')
+module.exports = app
---
+++ routes/api-routers/book.js
@@ -0,0 +1,120 @@
+/**
+ * restful api
+ * book 路由
+ * 1. 通过 id `?id={id}`精确查询一个条目, 或用 `?intro={id}`得到简介.
+ * 2. 通过 tag or author or title 查询多条内容, 只返回第一页数据(10条以内)
+ * 3. 通过 score, 筛选评分
+ * 4. 如果带上 count 则返回一个数字有多少条.
+ * 5. ?page={pageIndex} 得到该页的 documents
+ * 写于: 5/29/晴
+ */
+const express = require('express')
+const router = express.Router()
+
+// 引入数据库
+const { Books, BooksIntro } = require('../../src/db-utils')
+const { log, objectIsEmpty } = require('../../src/utils')
+const { getPage } = require('./_get_book_db_api')
+
+// *此处有一个坑, 浏览器发送跨域请求时, 会先自动发 options 探测一下..
+router.all('/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', 'PUT, OPTIONS, GET')
+  next()
+})
+
+router.options('/', (req, res) => {
+  res.sendStatus(200)
+})
+
+const urlQuery = async (query) => {
+  const queryBook = Books.find({})
+  // 查tag
+  if (query.hasOwnProperty('tag')) {
+    // queryBook.find({ tags: query.tag })
+    queryBook.find({ tags: { $regex: '.*' + query.tag + '.*' } })
+  }
+  // 查作者, 正则挺操蛋的..
+  if (query.hasOwnProperty('author')) {
+    queryBook.find({ 'info.作者': { $regex: '.*' + query.author + '.*' } })
+  }
+  // 查标题
+  if (query.hasOwnProperty('title')) {
+    queryBook.find({ title: { $regex: '.*' + query.title + '.*' } })
+  }
+  // 评分筛选
+  if (query.hasOwnProperty('score')) {
+    log('Number(query.score)', Number(query.score))
+    queryBook.find({ score: { $gte: Number(query.score) } })
+  }
+  // 如果只是想了解 数据有多少条 (count), 那便只返回 count咯~ 在分页之前
+  if (query.hasOwnProperty('count')) {
+    return queryBook.countDocuments()
+  }
+  // 到最后 总是要分页的 我不会一口把所有资料都吐给你 哈哈哈哈
+  if (query.hasOwnProperty('page')) {
+    return getPage(queryBook, query.page)
+  } else {
+    return getPage(queryBook, 1)
+  }
+}
+
+router.get('/', (req, res) => {
+  // http://127.0.0.1:3000/api/book
+  const query = req.query
+  if (objectIsEmpty(query)) {
+    const API_INFO = {
+      'msg': '请带上参数',
+      'get_book_by_id': 'http://127.0.0.1:3000/api/book?id={id}'
+    }
+    res.json(API_INFO)
+  } else {
+    log('req.query', query)
+    // id, 和得到此id intro 是排它性的: 既然查id 其他条件就不会生效
+    if (query.hasOwnProperty('id')) {
+      Books.find({ _id: query.id })
+        .then(r => {
+          // r 是数组
+          if (r.length) {
+            res.json(r[0])
+          } else {
+            res.json(null)
+          }
+        })
+      return
+    } else if (query.hasOwnProperty('intro')) {
+      BooksIntro.find({ _id: query.intro })
+        .then(r => {
+          // r 是数组
+          if (r.length) {
+            res.json(r[0])
+          } else {
+            res.json(null)
+          }
+        })
+      return
+    }
+    // 解析 url 的信息
+    // 这是一个很强的函数 (๑¯◡¯๑)~
+    urlQuery(query).then(r => {
+      res.json(r)
+    }).catch(err => {
+      log('err', err)
+    })
+  }
+})
+
+router.post('/', (req, res) => {
+  const body = req.body
+  log('body', body)
+  Books.findOne({ tags: body.content })
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      log('err', err)
+    })
+})
+
+module.exports = router
