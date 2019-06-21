6ceae9719dce1af4f282f451b78622078d978750
hulane <huulane@gmail.com>
2019-06-21 11:12:12 +0800

    remove ejs

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const path = require('path')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  // res.send('index', { title: '矩形图书馆' })
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
