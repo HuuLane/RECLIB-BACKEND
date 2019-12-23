64da200655b4237bb97e0652384e93287ddfd493
lane <huulane@gmail.com>
2019-12-23 10:32:31 +0800

    fix: the log

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
-const { log } = console
+const log = console.log.bind(console)
+
 const objectIsEmpty = (obj) => {
   return Object.keys(obj).length === 0 && obj.constructor === Object
 }
+
 module.exports = {
   log,
   objectIsEmpty
