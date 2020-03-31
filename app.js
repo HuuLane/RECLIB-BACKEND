92dfb3f39f0df0b915fe4404e3a15a1d74a510c9
lane <huulane@gmail.com>
2020-03-31 19:44:39 +0800

    fix: remove redundant endpoint index

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// 设置路由
-app.use('/', require('./routes/'))
+// Router
 app.use('/signup', require('./routes/signup.js'))
 app.use('/login', require('./routes/login.js'))
 app.use('/api', require('./routes/api.js'))
