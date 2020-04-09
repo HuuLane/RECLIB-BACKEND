69a9874cffe42b334a89cfc8225024ad0413a3dc
lane <huulane@gmail.com>
2020-04-09 11:48:31 +0800

    feat: Using await-to-js

--- routes/profile.js
+++ routes/profile.js
@@ -1,20 +1,24 @@
 const User = require('../Model/User')
 const express = require('express')
 const router = express.Router()
+const { logger } = require('../utils')
+const to = require('await-to-js').default
 
 router.get('/', async (req, res, next) => {
   const { uid } = req.session
-  // 没有登录
   if (!uid) {
     return res.json({ code: 0, msg: '请先登录' })
   }
-  try {
-    const data = await User.findOne({
+  const [err, data] = await to(
+    User.findOne({
       _id: uid
     }).select('date name index activity')
-    return res.json({ code: 1, msg: '成功读取用户数据', data })
-  } catch (error) {
-    return res.json({ code: 3, msg: '没有该用户, 有疑问请联系我!' })
+  )
+  if (data) {
+    res.json({ code: 1, msg: '成功读取用户数据', data })
+  } else {
+    res.json({ code: 3, msg: '没有该用户' })
+    logger.error(err)
   }
 })
 
