35183b7dbbcf76f21e61a3f099d9ed010180797a
lane <huulane@gmail.com>
2020-04-09 17:03:41 +0800

    feat: signup then session login

--- routes/signup.js
+++ routes/signup.js
@@ -1,35 +1,39 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../Model/User')
+const { logger } = require('../utils')
+const to = require('await-to-js').default
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
   const u = new User({
     email: req.body.email,
     password: req.body.password,
     name: req.body.name
   })
 
-  u.save()
-    .then(response => {
+  const [err, data] = await to(u.save())
+
+  if (err) {
+    if (err.code === 11000) {
       res.json({
-        code: 1,
-        msg: '创建账户成功'
+        code: 2,
+        msg: '用户名 或者 邮箱已有人先'
       })
-    })
-    .catch(err => {
-      if (err.code === 11000) {
-        res.json({
-          code: 2,
-          msg: '用户名 或者 邮箱已有人先'
-        })
-        return
-      }
+    } else {
       res.json({
         code: 0,
-        msg: '创建账户失败, 请联系我'
+        msg: 'Fail to sign up'
       })
-      console.error('*signup', err)
+      logger.error('Fail to signup', err)
+    }
+  } else {
+    req.session.name = data.name
+    req.session.uid = data._id.toString()
+    res.json({
+      code: 1,
+      msg: 'Create account successfully'
     })
+  }
 })
 
 module.exports = router
