ec303ca1d5fe7e2e45c8f04ab58e2894061929b8
lane <huulane@gmail.com>
2020-04-13 11:57:41 +0800

    refactor: db structure

---
+++ Model/Book.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const { logger } = require('../utils')
+
+const schema = new mongoose.Schema(
+  {
+    _id: String,
+    score: Number,
+    rating: Number,
+    title: String,
+    tags: [String],
+    imgUrl: String,
+    info: Object,
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ],
+    stock: {
+      total: Number,
+      loanout: [mongoose.Schema.Types.ObjectId]
+    }
+  },
+  {
+    versionKey: false
+  }
+)
+
+module.exports = mongoose.model('Book', schema, 'books')
