f44c64be9c4dd5a0284af99dcd69c0073fc2b9c7
lane <huulane@gmail.com>
2020-04-13 10:36:39 +0800

    refactor!: Comment model

--- Model/Comment.js
+++ Model/Comment.js
@@ -1,23 +1,19 @@
 const mongoose = require('mongoose')
 const { logger } = require('../utils')
+const User = require('../Model/User')
+const BookExtra = require('../Model/BookExtra')
 
 const schema = new mongoose.Schema(
   {
-    uid: {
-      type: mongoose.ObjectId,
-      required: true
+    bookID: {
+      type: String
     },
-    book: {
-      type: String,
-      required: true
+    uid: {
+      type: mongoose.Schema.Types.ObjectId
     },
     content: {
       type: String,
       required: true
-    },
-    index: {
-      type: Number,
-      unique: true
     },
     edited: {
       type: Boolean,
@@ -34,31 +30,32 @@ const schema = new mongoose.Schema(
 )
 
 schema.pre('save', async function (next) {
-  // only hash the password if it has been modified (or is new)
-  if (!this.isModified('password')) {
-    return next()
+  await Promise.all([
+    User.findByIdAndUpdate(this.uid, {
+      $push: { 'activity.comments': this._id }
+    }),
+    BookExtra.findByIdAndUpdate(
+      this.bookID,
+      { $push: { comments: this._id } },
+      { upsert: true, new: true }
+    )
+  ])
+  next()
+})
+
+schema.statics.getByBookID = async function (bookID) {
+  const b = await BookExtra.findById(bookID)
+  if (!b) {
+    return null
   }
-  logger.info('before sign up hash')
-  const hash = await bcrypt.hash(this.password, saltRounds)
-  logger.info('sign up hash', hash)
-  // override the cleartext password with the hashed one
-  this.password = hash
-  next()
-})
-
-schema.pre('save', async function (next) {
-  // for user index
-  const counter = await Counter.findOneAndUpdate(
-    { _id: 'forUserIndex' },
-    { $inc: { seq: 1 } },
-    { upsert: true }
-  )
-  this.index = counter.seq
-  next()
-})
-
-schema.methods.comparePasswd = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password)
+  return await this.find()
+    .where('_id')
+    .in(b.comments)
+    .exec()
 }
 
-module.exports = mongoose.model('User', schema, 'user')
+schema.methods.edit = async function () {
+  // TODO
+}
+
+module.exports = mongoose.model('Comment', schema, 'comments')
--- routes/comment.js
+++ routes/comment.js
@@ -1,14 +1,14 @@
-const { Books, StockAndCommit } = require('../db')
-const User = require('../Model/User')
+const { Books } = require('../db')
+const Comment = require('../Model/Comment')
+
 const { logger } = require('../utils')
 const router = require('express').Router()
 
 router.post('/', async (req, res) => {
   const { name, uid } = req.session
   if (!name) {
-    return res.json({ code: 0, msg: '请先登录' })
+    return res.json({ code: 0, msg: 'No login' })
   }
-  // @params: _id: 书的id, content: 评论内容
   const { id: bookID, content } = req.body
 
   // retrive book
@@ -17,59 +17,32 @@ router.post('/', async (req, res) => {
     return res.json({ code: 4, msg: 'no the book' })
   }
 
-  const date = Date.now()
-  // 存贮到 document of book 的数据
-  const dataOfBook = {
-    name,
-    userID: uid,
-    content,
-    date
-  }
-  // 存贮到 document of User 的数据
-  const dataOfUser = {
-    bookID,
-    content,
-    date,
-    bookName: b.title
-  }
   try {
-    // !两头存数据
-    await Promise.all([
-      User.updateOne(
-        { _id: uid },
-        { $push: { 'activity.comments': dataOfUser } }
-      ),
-      StockAndCommit.updateOne(
-        { _id: bookID },
-        { $push: { comments: dataOfBook } }
-      )
-    ])
-    res.json({ code: 1, msg: '评论成功' })
+    const c = new Comment({
+      uid,
+      bookID,
+      content
+    })
+    await c.save()
+    res.json({ code: 1, msg: 'Comment successful' })
   } catch (error) {
-    res.json({ code: 5, msg: '数据库发生错误, 请联系我' })
+    logger.error('comment', error)
+    res.json({ code: 5, msg: 'An error occurred in the database' })
   }
 })
 
 router.get('/:bookID', async (req, res) => {
-  // const { id: _id } = req.body
+  // TODO get all of comments of a user
   const _id = req.params.bookID
   if (!_id) {
-    return res.json({ code: 0, msg: '请<del>州长夫人..</del>带上参数!' })
+    return res.json({ code: 2, msg: 'No parameters' })
   }
-  const theBook = await StockAndCommit.findOne({
-    comments: { $exists: true },
-    _id
-  })
-  if (!theBook) {
-    return res.json({ code: 0, msg: '没有评论' })
+  const r = await Comment.getByBookID(_id)
+  if (r) {
+    res.json({ code: 1, msg: 'Get comments successful', comments: r })
+  } else {
+    res.json({ code: 0, msg: 'No comments' })
   }
-  // 现在可以安心 get comments
-  const { comments } = theBook
-  // 真没有..
-  if (!comments.length) {
-    return res.json({ code: 0, msg: '没有评论' })
-  }
-  res.json({ code: 1, msg: '读取评论成功', comments })
 })
 
 module.exports = router
