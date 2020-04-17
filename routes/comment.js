4bb64bd860c43e4154619f11a66780999ec19b0c
lane <huulane@gmail.com>
2020-04-17 16:44:40 +0800

    feat: able to EDIT comment

--- routes/comment.js
+++ routes/comment.js
@@ -47,4 +47,61 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const { name, uid } = req.session
+  const { content } = req.body
+
+  const c = await Comment.findById(id)
+
+  if (!c) {
+    return res.json({ code: 4, msg: 'no the comment' })
+  }
+
+  if (c.user.toString() !== uid) {
+    return res.json({
+      code: 3,
+      msg: 'user only able to edit his/her own comments'
+    })
+  }
+
+  c.edited = true
+  c.content = content
+  c.save()
+    .then(() => {
+      res.json({ code: 1, msg: 'edit the comment successfully' })
+    })
+    .catch(error => {
+      logger.error('edit a comment', error)
+      res.json({ code: 5, msg: 'An error occurred in the database' })
+    })
+})
+
+router.delete('/:id', async (req, res) => {
+  const { name, uid } = req.session
+
+  const c = await Comment.findById(id)
+
+  if (!c) {
+    return res.json({ code: 4, msg: 'no the comment' })
+  }
+
+  if (c.user.toString() !== uid) {
+    return res.json({
+      code: 3,
+      msg: 'user only able to delete his/her own comments'
+    })
+  }
+
+  c.deleted = true
+  c.save()
+    .then(() => {
+      res.json({ code: 1, msg: 'delete the comment successfully' })
+    })
+    .catch(error => {
+      logger.error('delete a comment', error)
+      res.json({ code: 5, msg: 'An error occurred in the database' })
+    })
+})
+
 module.exports = router
